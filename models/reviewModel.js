21726e300595c2f2a02929b9c5362d8c75de9cb8
Mohamed Talaat <Mohamed.Talaat.Afyfy@Gmail.com>
2023-05-26 11:20:57 +0200

    add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const typeCustomer = new mongoose.Schema({
+  _id: { type: mongoose.Types.ObjectId, ref: "Customer" },
+  name: { type: String, required: true },
+});
+
+const schema = new mongoose.Schema({
+  productId: { type: mongoose.Types.ObjectId, ref: "Product" },
+  customer: typeCustomer,
+  score: { type: Number, enum: [1, 2, 3, 4, 5] },
+  comment: String,
+});
+
+mongoose.model("Review", schema);
