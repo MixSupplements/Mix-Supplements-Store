4ca52a7c1bbe7e95fc3fac22c9a5360134da802f
Ahmed Tarek Helmy <ahmedtarek16102169@gmail.com>
2023-05-26 16:39:43 +0300

    add brands model

---
+++ models/brandModel.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+
+const brandSchema=new mongoose.Schema({
+name:{type:String,required:true},
+logo:{type:String,required:true},
+})
+
+
+mongoose.model("Brand",brandSchema);
+     
