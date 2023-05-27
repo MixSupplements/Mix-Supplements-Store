58f690f9595dc2d766e054b640071165cc50df24
Abdalla <abdallahali3399@gmail.com>
2023-05-27 20:02:12 +0300

    used adminRouter in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = require("./config").app;
 const express = require('express');
 const morgan = require('morgan');
 
+const adminRouter = require('./routes/adminRouter');
 
 const NotFoundMiddleware = require('./middlewares/NotFoundMiddleware');
 const ErrorMiddleware = require('./middlewares/ErrorMiddleware');
@@ -16,7 +17,7 @@ app.use(express.json());
 
 /***************** Routes ***************/
 
-
+app.use(adminRouter);
 
 app.use(NotFoundMiddleware);
 app.use(ErrorMiddleware);
