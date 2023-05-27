65db7912305f8e9cd12de69d03e4de5c48dc337d
Abdalla <abdallahali3399@gmail.com>
2023-05-27 17:59:43 +0300

    used brandRouter in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = require("./config").app;
 const express = require('express');
 const morgan = require('morgan');
 
-
+const brandRouter = require('./routes/brandRouter');
 const NotFoundMiddleware = require('./middlewares/NotFoundMiddleware');
 const ErrorMiddleware = require('./middlewares/ErrorMiddleware');
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 /***************** Routes ***************/
 
-
+app.use(brandRouter);
 
 app.use(NotFoundMiddleware);
 app.use(ErrorMiddleware);
