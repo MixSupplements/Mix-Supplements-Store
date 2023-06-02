e78076a1227e3e25f10f6e42c12110c515e3c7e3
ahmad-alkahky <ahmadalkahky@gmail.com>
2023-06-02 18:20:56 +0300

    fix: validation error handling

--- middlewares/validations/errorHandler.js
+++ middlewares/validations/errorHandler.js
@@ -1,14 +1,15 @@
 const { validationResult } = require("express-validator");
 
 module.exports = (request, response, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(request);
 
     if (!errors.isEmpty())
     {
+        console.log(errors);
         const error = new Error('Validation failed');
         error.status = 422;
-        error.errors = errors.array();
-        return next(error);
+        error.errors = errors.array().map((error => error = error.msg));
+        next(error);
     }
     next();
 };
