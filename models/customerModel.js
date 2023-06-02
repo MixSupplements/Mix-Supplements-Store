a463c5a2ed8598c4675b8cee4222e5e38f0b6210
ahmad-alkahky <ahmadalkahky@gmail.com>
2023-06-02 16:06:08 +0300

    fix: add password hashing on update to customer

--- models/customerModel.js
+++ models/customerModel.js
@@ -25,8 +25,9 @@ const customerSchema = new mongoose.Schema({
     timestamps: true
 });
 
-module.exports = mongoose.model('Customer', customerSchema);
-
+/**
+ * encrypt the password when the customer data are first saved on Database
+ */
 customerSchema.pre('save', async function (next) {
     if (!this.isModified('password'))
         return next();
@@ -38,3 +39,26 @@ customerSchema.pre('save', async function (next) {
         next();
     } catch (error) { next(error); }
 });
+
+/**
+ * encrypt the password when the customer data are updated
+ */
+customerSchema.pre('updateOne', async function (next) {
+    const update = this.getUpdate();
+    if (update.$set.password)
+    {
+        try
+        {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(update.$set.password, salt);
+            this.setUpdate({ $set: { password: hashedPassword } });
+
+        } catch (error)
+        {
+            next(error);
+        }
+    }
+    next();
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
