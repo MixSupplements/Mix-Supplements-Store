6a3d2401ee1a41045196cc56b4c4f4d309e55db2
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2023-06-06 23:00:09 +0300

    modify brand model

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,51 +1,45 @@
-const express = require('express');
-const  mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
 const Brands = mongoose.model('Brand');
 
 exports.getBrands = (req, res, next) => {
     Brands.find({})
-    .then(data => {
-        res.status(200).json(data);
-    })
-    .catch(err => next(err))
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => next(err))
 }
 
 exports.postBrand = (req, res, next) => {
-    let {name, logo} = req.body;
-
-    let newBrand = new Brands({
-        name: name,
-        logo: logo
-    })
-
+    let { name } = req.body;
+    let newBrand = new Brands({ name: name })
     newBrand.save()
-    .then(data => {
-        res.status(200).json({"message": "brand added successfully"})
-    })
-    .catch(err => next(err))
+        .then(data => {
+            res.status(200).json({ "message": "brand added successfully", data })
+        })
+        .catch(err => next(err))
 }
 
 exports.getBrandById = (req, res, next) => {
     Brands.findById(req.params.id)
-    .then(data => {
-        res.status(200).json(data);
-    })
-    .catch(err => next(err))
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => next(err))
 }
 
 exports.patchBrand = (req, res, next) => {
-    Brands.findByIdAndUpdate(req.params.id, req.body)
-    .then(data => {
-        res.status(200).json({"message": "brand updated successfully"});
-    })
-    .catch(err => next(err))
+    Brands.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(data => {
+            res.status(200).json({ "message": "brand updated successfully", data });
+        })
+        .catch(err => next(err))
 }
 
 exports.deleteBrand = (req, res, next) => {
-    Brands.deleteOne({_id: req.params.id})
-    .then(data => {
-        res.status(200).json({"message": "brand deleted Successfully"})
-    })
-    .catch(err => next(err))
+    Brands.deleteOne({ _id: req.params.id })
+        .then(data => {
+            res.status(200).json({ "message": "brand deleted Successfully", data })
+        })
+        .catch(err => next(err))
 }
