2811e785f2fcdfe045ae040bca5028d8bca4f02d
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2023-06-06 22:59:52 +0300

    use upload router

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = require("./config").app;
 
 const express = require("express");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 
 // Routers Layer
 const categoryRouter = require("./routes/categoresRouter");
 const shippingFeesRouter = require("./routes/shippingFeesRouter");
-const orderRouter=require("./Routes/orderRoute");
+const orderRouter = require("./Routes/orderRoute");
 const customerRouter = require('./routes/customerRoutes');
 const productRouter = require('./routes/productRoute');
 const brandRouter = require('./routes/brandRouter');
 const cartRouter = require('./routes/cartRouter');
 const adminRouter = require('./routes/adminRouter');
 const wishlistRouter = require('./routes/wishlistRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const NotFoundMiddleware = require("./middlewares/NotFoundMiddleware");
 const ErrorMiddleware = require("./middlewares/ErrorMiddleware");
@@ -33,6 +33,7 @@ app.use(cartRouter);
 app.use(brandRouter);
 app.use(adminRouter);
 app.use(wishlistRouter);
+app.use(uploadRouter);
 
 app.use(NotFoundMiddleware);
 app.use(ErrorMiddleware);
