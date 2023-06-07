20bf16bdca1719757b33e58146c6f1ee2275a6c7
Ahmed Tarek Helmy <ahmedtarek16102169@gmail.com>
2023-06-07 21:23:58 +0300

    reviewModel

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const typeCustomer = new mongoose.Schema({
   _id: { type: mongoose.Types.ObjectId, ref: "Customer" },
-  name: { type: String, required: true },
+  name: { type: String, required: true }
 });
 
 const schema = new mongoose.Schema({
   productId: { type: mongoose.Types.ObjectId, ref: "Product" },
   customer: typeCustomer,
   score: { type: Number,required: true , enum: [1, 2, 3, 4, 5] },
-  comment: {type: String ,required: true },
+  comment: {type: String ,required: true }
 });
 
 mongoose.model("Review", schema);
