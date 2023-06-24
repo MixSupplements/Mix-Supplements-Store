fda8d3a81a244c6b21a7810d350859a26967bb08
ahmad-alkahky <ahmadalkahky@gmail.com>
2023-06-24 16:49:54 +0300

    minor changes

--- models/shippingDestinationModel.js
+++ models/shippingDestinationModel.js
@@ -4,7 +4,7 @@ const ShippingDestinationSchema = new mongoose.Schema({
   governorate: { type: String, required: true, },
   fees: { type: Number, required: true, },
   active: { type: Boolean, required: true, default: true },
-  shippingTime: { type: String, required: true, },
+  shippingDays: { type: Number, required: true, },
 });
 
 module.exports = mongoose.model("ShippingDestination", ShippingDestinationSchema);
